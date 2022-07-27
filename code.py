452aa376f54a4c2b5fb48fc7e6f0a3a248ae61eb
“kamesh” <kamesh20k@gmail.com>
2022-07-27 03:31:21 +0000

    devops201:3rdcommit

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ c = 7
 # c = float(input('Enter third side: '))
 
 # calculate the semi-perimeter
-s = (a + b + c) / 2
+s = (a + b + c) / 3
 
 # calculate the area
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
